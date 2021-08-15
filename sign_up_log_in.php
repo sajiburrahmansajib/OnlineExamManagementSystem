1fbf0cd9b138ae509e07bd7819dd4ebf4bf7ac17
md. shahriar ovi <ovishahriar736@gmail.com>
2021-08-16 03:10:11 +0600

    student sign up form created

---
+++ sign_up_log_in.php
@@ -0,0 +1,68 @@
+<?php include_once("connection.php"); ?>
+<?php
+	session_start();
+	if (isset($_SESSION['user_id'])) {
+      header('Location: userhome.php');
+      exit;
+  	}
+?>
+
+<?php
+
+$u_id="";
+if(isset($_POST['submit'])){
+
+		if (empty($_POST['user_name']) || empty($_POST['password']) || empty($_POST['phone']) || empty($_POST['mail']) || empty($_POST['address'])){
+
+			echo "<script type='text/javascript'>alert('Field Is Empty!')</script>";
+		}
+                else{
+                    
+                    $mail = $_POST['mail'];
+			
+
+			if (!filter_var($mail, FILTER_VALIDATE_EMAIL))
+                                {
+                                echo "<script type='text/javascript'>alert('Invalid email format!')</script>";
+                                }
+								$sql = " SELECT COUNT(*) FROM user WHERE(mail='".$mail."') ";
+
+			$qury = mysqli_query($conn, $sql);
+
+			$result = mysqli_fetch_array($qury);
+			
+			if($result[0]>0)
+            {
+                echo "<script type='text/javascript'>alert('This E-mail Is Already Used! Please Try Another E-Mail')</script>";				
+            }
+			$phone = $_POST['phone'];
+			$sql = " SELECT COUNT(*) FROM user WHERE(phone='".$phone."') ";
+
+			$qury = mysqli_query($conn, $sql);
+
+			$result = mysqli_fetch_array($qury);
+			
+			if($result[0]>0)
+            {
+                echo "<script type='text/javascript'>alert('This Phone Number Is Already Used! Please Try Another Number')</script>";				
+            }
+                        else{            
+			$name = $_POST['user_name'];
+                        $password= MD5($_POST['password']);
+                        
+			$address = $_POST['address'];
+                        $sex = $_POST['gender'];
+			
+
+			$sql = "INSERT INTO user (user_name, password, phone, mail, address,sex)
+				VALUES ('".$name."','".$password."','".$phone."','".$mail."','".$address."','".$sex."')";
+
+				if (mysqli_query($conn, $sql)) {
+				     echo "<script type='text/javascript'>alert('Welcome! Register Successfully')</script>";
+				} else {
+				    echo "<script type='text/javascript'>alert('Failed!')</script>";
+				}
+                        }
+		}
+	}
+?>
