f65aeaf080488796c6dd62950bed0f1b50f03adb
sajjadrocky <shossain163082@bscse.uiu.ac.bd>
2021-08-16 04:01:57 +0600

    login form added

--- sign_up_log_in.php
+++ sign_up_log_in.php
@@ -66,3 +66,156 @@ if(isset($_POST['submit'])){
 		}
 	}
 ?>
+<?php 
+    if(isset($_POST['submit1'])){
+
+		if (empty($_POST['mail']) || empty($_POST['password1'])){
+
+			echo "<script type='text/javascript'>alert('Field Is Empty!')</script>";
+		}
+                else
+                {   $user_name=$_POST['mail'];
+                    $pass = MD5($_POST['password1']);
+
+      			$sql = " SELECT COUNT(*) FROM user WHERE( mail='".$user_name."' AND password='".$pass."') ";
+
+				$qury = mysqli_query($conn, $sql);
+
+				$result = mysqli_fetch_array($qury);
+			
+				if($result[0]>0)
+                                {
+                                        $sessionSql = " SELECT user_id,user_name,password FROM user WHERE ( mail='".$user_name."' AND password='".$pass."') ";
+					$sessionQury = mysqli_query($conn, $sessionSql);
+					while($sessionResult = mysqli_fetch_array($sessionQury, MYSQLI_BOTH)){
+						 $u_id = $sessionResult['user_id'];
+						 $u_name = $sessionResult['user_name'];
+					}
+					$_SESSION['user_id'] = $u_id;
+					$_SESSION['user_name'] = $u_name;
+			
+					header('location: userhome.php');
+					exit;				
+                                    }
+                                    else
+                                    {
+                                            echo "<script type='text/javascript'>alert('Invalid User Name or Password!')</script>";
+                                    }
+                                }
+                }
+     			
+?><?php 
+    if(isset($_POST['submit1'])){
+
+		if (empty($_POST['mail']) || empty($_POST['password1'])){
+
+			echo "<script type='text/javascript'>alert('Field Is Empty!')</script>";
+		}
+                else
+                {   $user_name=$_POST['mail'];
+                    $pass = MD5($_POST['password1']);
+
+      			$sql = " SELECT COUNT(*) FROM user WHERE( mail='".$user_name."' AND password='".$pass."') ";
+
+				$qury = mysqli_query($conn, $sql);
+
+				$result = mysqli_fetch_array($qury);
+			
+				if($result[0]>0)
+                                {
+                                        $sessionSql = " SELECT user_id,user_name,password FROM user WHERE ( mail='".$user_name."' AND password='".$pass."') ";
+					$sessionQury = mysqli_query($conn, $sessionSql);
+					while($sessionResult = mysqli_fetch_array($sessionQury, MYSQLI_BOTH)){
+						 $u_id = $sessionResult['user_id'];
+						 $u_name = $sessionResult['user_name'];
+					}
+					$_SESSION['user_id'] = $u_id;
+					$_SESSION['user_name'] = $u_name;
+			
+					header('location: userhome.php');
+					exit;				
+                                    }
+                                    else
+                                    {
+                                            echo "<script type='text/javascript'>alert('Invalid User Name or Password!')</script>";
+                                    }
+                                }
+                }
+     			
+?><?php 
+    if(isset($_POST['submit1'])){
+
+		if (empty($_POST['mail']) || empty($_POST['password1'])){
+
+			echo "<script type='text/javascript'>alert('Field Is Empty!')</script>";
+		}
+                else
+                {   $user_name=$_POST['mail'];
+                    $pass = MD5($_POST['password1']);
+
+      			$sql = " SELECT COUNT(*) FROM user WHERE( mail='".$user_name."' AND password='".$pass."') ";
+
+				$qury = mysqli_query($conn, $sql);
+
+				$result = mysqli_fetch_array($qury);
+			
+				if($result[0]>0)
+                                {
+                                        $sessionSql = " SELECT user_id,user_name,password FROM user WHERE ( mail='".$user_name."' AND password='".$pass."') ";
+					$sessionQury = mysqli_query($conn, $sessionSql);
+					while($sessionResult = mysqli_fetch_array($sessionQury, MYSQLI_BOTH)){
+						 $u_id = $sessionResult['user_id'];
+						 $u_name = $sessionResult['user_name'];
+					}
+					$_SESSION['user_id'] = $u_id;
+					$_SESSION['user_name'] = $u_name;
+			
+					header('location: userhome.php');
+					exit;				
+                                    }
+                                    else
+                                    {
+                                            echo "<script type='text/javascript'>alert('Invalid User Name or Password!')</script>";
+                                    }
+                                }
+                }
+     			
+?><?php 
+    if(isset($_POST['submit1'])){
+
+		if (empty($_POST['mail']) || empty($_POST['password1'])){
+
+			echo "<script type='text/javascript'>alert('Field Is Empty!')</script>";
+		}
+                else
+                {   $user_name=$_POST['mail'];
+                    $pass = MD5($_POST['password1']);
+
+      			$sql = " SELECT COUNT(*) FROM user WHERE( mail='".$user_name."' AND password='".$pass."') ";
+
+				$qury = mysqli_query($conn, $sql);
+
+				$result = mysqli_fetch_array($qury);
+			
+				if($result[0]>0)
+                                {
+                                        $sessionSql = " SELECT user_id,user_name,password FROM user WHERE ( mail='".$user_name."' AND password='".$pass."') ";
+					$sessionQury = mysqli_query($conn, $sessionSql);
+					while($sessionResult = mysqli_fetch_array($sessionQury, MYSQLI_BOTH)){
+						 $u_id = $sessionResult['user_id'];
+						 $u_name = $sessionResult['user_name'];
+					}
+					$_SESSION['user_id'] = $u_id;
+					$_SESSION['user_name'] = $u_name;
+			
+					header('location: userhome.php');
+					exit;				
+                                    }
+                                    else
+                                    {
+                                            echo "<script type='text/javascript'>alert('Invalid User Name or Password!')</script>";
+                                    }
+                                }
+                }
+     			
+?>
