8ed1e19039375bc6add30109d500abdde7589c81
sajibur rahman <mrahman172152@bscse.uiu.ac.bd>
2021-08-16 02:30:41 +0600

    Interface code created

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles/w3.css">
+        <title>Online Exam System</title>
+    </head>
+    <style> 
+    .bgimg {
+    
+    background-size: 100% 800px;
+    background-repeat: no-repeat;
+               }
+
+     </style>
+    <body class="bgimg">
+        <ul class="w3-navbar w3-black">
+        <li><a class="w3-text-red" href="#">Home</a></li>
+		<li><a href="userhome.php">Exam</a></li>
+        <li><a href="about_us.php">About Us</a></li>
+        <li class="w3-right w3-teal"><a href="admin.php">Teacher</a></li>
+        <li class="w3-right w3-teal"><a href="sign_up_log_in.php">Student</a></li>
+    </ul>
+        
+        <div class="w3-container w3-display-topmiddle w3-padding-jumbo w3-margin-top">
+            <div>
+                <h1 > Online Exam System </h1>
+                <h5 class="w3-animate-right w3-xlarge w3-center font w3-text-black">We Make Learning Better... </h5>
+            </div>   
+        </div>
+       
+        
+    </body>
+    
+</html>
