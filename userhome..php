f6664f121b767c0a3547e1833152c2c147a67518
munnaahamed <mahamed172146@bscse.uiu.ac.bd>
2021-08-16 12:02:24 +0600

    student interface created

--- userhome..php
+++ userhome..php
@@ -35,3 +35,35 @@ if(isset($_POST['submit2']))
     }
 	
 ?>
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles/w3.css">
+        <title>Online Exam System</title>
+    </head>
+    <style> 
+        .bgimg {
+    background-image:url('./image/paper.jpg');
+    background-size: 103% 920px;
+    background-repeat: no-repeat;
+               }
+    .font {
+    font-family: "Comic Sans MS", cursive, sans-serif;
+    }
+
+     </style>
+    <body class="bgimg">
+        <ul class="w3-navbar w3-black">
+        <li><a class="w3-text-blue" href="#"><?php echo $_SESSION['user_name'];?></a></li>
+            
+        <li class="w3-dropdown-hover"><a href="#">Edit Profile<i class="fa fa-caret-down"></i></a>
+      <div class="w3-dropdown-content w3-white w3-card-4">
+                <a href="update_general.php">General</a>
+                <a href="update_password.php">Update Password</a>
+          </div>
+    </li>
+        
+        <li class="w3-right w3-teal"><a href="logout.php">Log Out</a></li>
+    </ul>
