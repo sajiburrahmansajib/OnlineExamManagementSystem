fead0163c4fa23a2ade66fe0f95c469986107360
sajibur rahman <mrahman172152@bscse.uiu.ac.bd>
2021-08-16 10:59:10 +0600

    exam type part addad in userhome part

--- userhome..php
+++ userhome..php
@@ -5,3 +5,33 @@
       header('Location: sign_up_log_in.php');
       exit;
   	}
+  	?>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+</style>
+
+
+
+<?php
+$errorMassage ="";
+$succ = "";
+$emailErr="";
+$u_id="";
+if(isset($_POST['submit2']))
+    { 
+    $selected_val1 = $_POST['take'];
+    $selected_val2 = $_POST['difficulty'];
+    $_SESSION['course']=$selected_val1;
+        header("Location:test.php?id=$selected_val1&name=$selected_val2");
+        exit;
+		
+    }
+    else
+    {
+        
+    }
+	
+?>
